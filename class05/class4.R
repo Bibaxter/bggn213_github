f83f85ecbbfe077cc323ff43de581d38d363fe60
Bryn Baxter <bibaxter@ucsd.edu>
2025-02-26 14:58:05 -0800

    add class 5-8

---
+++ class05/class4.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "My first R Script report"
+#' author: "Bryn Baxter"
+#' date: "January 22, 2025"
+#' ---
+
+
+
+
+#this is just a txt file
+plot(1:10)
+
+x <- 1:50
+plot(x, sin(x),type = "l", col="blue", lwd=3, xlab="stop-now")
+
